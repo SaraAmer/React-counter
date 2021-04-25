3954072a911f97d5dae428b72af965c4de9b3667
Sara Amer <saraamersaad8@gmail.com>
2021-04-25 19:46:52 +0200

    react counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+class App extends React.Component {
+
+  render()
+  {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Counter initCounter ={0}/>
     </div>
-  );
+  );}
+}
+class Counter extends React.Component{
+  constructor(props)
+  {
+    super();
+    this.state={
+      counter : props.initCounter,
+    }
+  }
+  increment=()=>{
+  
+    this.state.counter=this.setState({counter:this.state.counter+1});
+  }
+  decrement=()=>{
+    
+    this.state.counter=this.setState({counter:this.state.counter-1});
+  }
+
+  render(){
+    let style={
+      
+      margin:"auto",
+      marginTop:"30px",
+    };
+    return (
+   <div > 
+   <div style={style}>
+   <button onClick={this.decrement} style={{cursor:"pointer"}} >
+     -
+   </button>
+   <span style={{padding:"20px",}}>
+   {this.state.counter}
+   </span>
+   <button onClick={this.increment} style={{cursor:"pointer"}}>
+    +
+   </button>
+   </div>
+   </div>  
+    );
+  }
+ 
 }
 
 export default App;
